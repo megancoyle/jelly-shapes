efb20b007ac4532356e79434fce4c0c275c65639
Megan Coyle <mcoyle@usgbc.org>
2017-03-08 11:55:54 -0500

    Add jelly functionality

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var options = {
+  paths: '#pentagon-path',
+  pointsNumber: 10,
+  maxDistance: 70,
+  color: '#4dc3ff',
+  centroid: '.centroid-text'
+};
+
+var jelly = new Jelly('.jelly-canvas', options);
